b5c82dad1ee74de46422e616db131f8f4c7ebe0a
Bishwanath821 <109907773+Bishwanath821@users.noreply.github.com>
2025-03-11 10:32:32 +0000

    learning variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail="bishwanathmahato821@gmail.com"
+var accountPassword ="12345"
+AccountCity ="Jaipur"
+let accountState;
+
+//accountId=2  //not allowed 
+accountEmail="chai@gmail.com"
+accountPassword="5898"
+AccountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,AccountCity,accountState])
+
+/*
+prefer not to use var because ofissue in block scope and functional scope
+*/
